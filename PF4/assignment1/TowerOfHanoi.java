bf557f5c35e5c45889d1f52b35295b66ca540ec6
meta-shreya-bordia <shreya.bordia@metacube.com>
2017-07-24 18:24:35 +0530

    PF Session 4 19/7/17

--- PF4/assignment1/TowerOfHanoi.java
+++ PF4/assignment1/TowerOfHanoi.java
@@ -3,8 +3,17 @@ package assignment1;
 import java.util.ArrayList;
 import java.util.List;
 
+/**Tower of Hanoi problem deals with transfering a plank of smaller size from source to destination
+* using a temporary stack
+* when only one peg can be moved at one time
+* and larger peg cant be placed onto smaller peg
+**/
 public class TowerOfHanoi {
-
+	/**@Method	towerOfHanoi(String source, String destination, String temp, int numOfDisk)
+	* solution for moving 1 peg from source to destination
+	* can be extended for some k pegs
+	* which can be generalised for any number of pegs
+	*/
 	List<String> towerOfHanoi(String source, String destination, String temp, int numOfDisk){
 		if(numOfDisk <= 0)
 			throw new IllegalArgumentException();
@@ -20,7 +29,8 @@ public class TowerOfHanoi {
 			
 			return moves;
 		}
-		 
+		
+		// recursive addition of all steps to be moved to destination
 		moves.addAll(towerOfHanoi(source, temp, destination, numOfDisk-1));
 		moves.add("Move Disk "+numOfDisk+" from "+source+" to "+destination);
 		moves.addAll(towerOfHanoi(temp, destination, temp, numOfDisk-1));
@@ -28,6 +38,8 @@ public class TowerOfHanoi {
 		return moves;
 	}
 	
+	/** Testing for 1 additional peg when blocks are 3
+	*/
 	public static void main(String[] args){
 		TowerOfHanoi t = new TowerOfHanoi();
 		
@@ -38,12 +50,5 @@ public class TowerOfHanoi {
 		for(int i=0; i< output.size(); i++){
 			System.out.println(output.get(i));
 		}
-		
-		/**output = t.towerOfHanoi("A", "B", "C", 12); //Overrides strings
-		
-		System.out.println("Number of moves: "+ output.size());
-		for(int i=0; i< output.size(); i++){
-			System.out.println(output.get(i));
-		}**/
 	}
 }
